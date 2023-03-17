32ae14c3e1fe28170fe724964dfee0af9c809552
Alek Jarmov <alek.jarmov@gmail.com>
2023-03-17 20:23:05 +0100

    Change folder structure

---
+++ backend/src/test/java/mk/finki/ukim/emt/emt/EmtApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.emt.emt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
