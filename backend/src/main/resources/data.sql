291330e08fcfb004bb6dc42a370484b16caa3c84
Alek Jarmov <alek.jarmov@gmail.com>
2023-03-20 00:42:49 +0100

    Small bugfix

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,8 +3,8 @@ INSERT INTO country (id, name, continent) VALUES (2, 'USA', 'Americas');
 INSERT INTO country (id, name, continent) VALUES (3, 'China', 'Asia');
 INSERT INTO country (id, name, continent) VALUES (4, 'Japan', 'Asia');
 
-INSERT INTO author (id, name, surname, country) VALUES (3, 'Victor', 'Hugo', 1);
-INSERT INTO author (id, name, surname, country) VALUES (1, 'John', 'Smith', 2);
-INSERT INTO author (id, name, surname, country) VALUES (2, 'Mark', 'Twain', 2);
-INSERT INTO author (id, name, surname, country) VALUES (4, 'Mo', 'Yan', 3);
-INSERT INTO author (id, name, surname, country) VALUES (5, 'Jin', 'Yong', 4);
+INSERT INTO author (id, name, surname, country_id) VALUES (1, 'Victor', 'Hugo', 1);
+INSERT INTO author (id, name, surname, country_id) VALUES (2, 'John', 'Smith', 2);
+INSERT INTO author (id, name, surname, country_id) VALUES (3, 'Mark', 'Twain', 2);
+INSERT INTO author (id, name, surname, country_id) VALUES (4, 'Mo', 'Yan', 3);
+INSERT INTO author (id, name, surname, country_id) VALUES (5, 'Jin', 'Yong', 4);
